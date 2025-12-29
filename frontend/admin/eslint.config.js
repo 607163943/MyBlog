eeaafd242bd8e2dd63ca4600d314d686bfda0b8a
607163943 <q45962219@126.com>
2025-12-29 12:12:27 +0800

    迁移管理端首页UI

--- frontend/admin/eslint.config.js
+++ frontend/admin/eslint.config.js
@@ -3,11 +3,22 @@ import globals from 'globals'
 import js from '@eslint/js'
 import pluginVue from 'eslint-plugin-vue'
 import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 
 export default defineConfig([
   {
     name: 'app/files-to-lint',
     files: ['**/*.{vue,js,mjs,jsx}'],
+    rules: {
+      'vue/multi-word-component-names': [
+        'warn',
+        {
+          ignores: ['index'] // vue组件名称多单词组成（忽略index.vue）
+        }
+      ],
+      'vue/no-setup-props-destructure': ['off'], // 关闭 props 解构的校验
+      'no-undef': 'error'
+    }
   },
 
   globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
@@ -15,13 +26,14 @@ export default defineConfig([
   {
     languageOptions: {
       globals: {
-        ...globals.browser,
-      },
-    },
+        ...globals.browser
+      }
+    }
   },
 
   js.configs.recommended,
   ...pluginVue.configs['flat/essential'],
 
   skipFormatting,
+  eslintPluginPrettierRecommended
 ])
